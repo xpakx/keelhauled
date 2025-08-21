90b4249f57b90747b04f6787738efb6fc35ecd9e
xpakx <55586470+xpakx@users.noreply.github.com>
2025-08-21 15:06:22 +0200

    feat: draw grid

--- script.ts
+++ script.ts
@@ -2,6 +2,9 @@ class Game {
 	context: CanvasRenderingContext2D;
 	canvas: HTMLCanvasElement;
 	prevTimestamp: number = 0;
+	cellSize = 70;
+
+	gridSize = 5;
 
 	constructor(context: CanvasRenderingContext2D, canvas: HTMLCanvasElement) {
 		this.context = context;
@@ -9,6 +12,35 @@ class Game {
 	}
 
 	nextFrame(_timestamp: number) {
+		this.drawGrid();
+	}
+
+	drawGrid() {
+		this.context.save();
+		const gridPixelWidth = this.cellSize*this.gridSize;
+		const gridPixelHeight = this.cellSize*this.gridSize;
+		const offsetX = (this.canvas.width - gridPixelWidth) / 2;
+		const offsetY = (this.canvas.height - gridPixelHeight) / 2;
+
+		this.context.translate(offsetX + 0.5, offsetY + 0.5);
+		for (let i = 0; i <= this.gridSize; i++) {
+			const x = i * this.cellSize;
+			this.context.beginPath();
+			this.context.moveTo(x, 0);
+			this.context.lineTo(x, gridPixelWidth);
+			this.context.strokeStyle = "#aaa";
+			this.context.stroke();
+		}
+
+		for (let j = 0; j <= this.gridSize; j++) {
+			const y = j * this.cellSize;
+			this.context.beginPath();
+			this.context.moveTo(0, y);
+			this.context.lineTo(gridPixelHeight, y);
+			this.context.strokeStyle = "#aaa";
+			this.context.stroke();
+		}
+		this.context.restore();
 	}
 }
 
@@ -24,8 +56,8 @@ window.onload = async () => {
 		return;
 	}
 
-	canvas.width = 300;
-	canvas.height = 200;
+	canvas.width = 800;
+	canvas.height = 600;
 	let game = new Game(context, canvas);
 
 	const frame = (timestamp: number) => {
