08a05bba26cf22f48ca416acc27ebc8dc79b7f0a
xpakx <55586470+xpakx@users.noreply.github.com>
2025-08-29 17:44:21 +0200

    feat: let user load sprite sheets

--- classes/rules.ts
+++ classes/rules.ts
@@ -54,6 +54,36 @@ export class DefaultCardLoader implements CardLoader {
 			image.onerror = reject;
 		});
 	}
+
+	async splitGridImage(img: HTMLImageElement, rows: number, cols: number) {
+		const cellWidth = img.width / cols;
+		const cellHeight = img.height / rows;
+		const cells = [];
+		const canvas = new OffscreenCanvas(cellWidth, cellHeight);
+		const ctx = canvas.getContext("2d");
+		if (!ctx) throw new Error("No 2D context!");
+
+		for (let y = 0; y < rows; y++) {
+			for (let x = 0; x < cols; x++) {
+				ctx.clearRect(0, 0, canvas.width, canvas.height);
+				ctx.drawImage(
+					img,
+					x * cellWidth, y * cellHeight,
+					cellWidth, cellHeight,
+					0, 0,
+					cellWidth, cellHeight
+				);
+
+				const blob = await canvas.convertToBlob();
+				const cellImg = new Image();
+				cellImg.src = URL.createObjectURL(blob);
+				cellImg.onload = () => URL.revokeObjectURL(cellImg.src);
+				cells.push(cellImg);
+			}
+		}
+
+		return cells;
+	}
 }
 
 
