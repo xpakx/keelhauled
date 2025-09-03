9158d0d7fe51eb15e497796e1be63d7d7ad63fd1
xpakx <55586470+xpakx@users.noreply.github.com>
2025-09-03 19:21:56 +0200

    refactor: use Fisher-Yates for shuffling

--- classes/card-lib.ts
+++ classes/card-lib.ts
@@ -82,7 +82,10 @@ export class Deck implements CardProducer {
 	}
 
 	shuffle() {
-		this.cards.sort(() => Math.random() - 0.5);
+		for (let i = this.cards.length - 1; i > 0; i--) {
+			const j = Math.floor(Math.random() * (i + 1));
+			[this.cards[i], this.cards[j]] = [this.cards[j], this.cards[i]];
+		}
 	}
 
 	getCard(name: string): Card | undefined {
