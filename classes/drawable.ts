a9ea0cd151dbd9c1ad457c865520cd70a6761d87
xpakx <55586470+xpakx@users.noreply.github.com>
2025-09-09 14:26:11 +0200

    feat: create drawables for interface and basic button

--- classes/drawable.ts
+++ classes/drawable.ts
@@ -16,6 +16,16 @@ export interface Drawable<T, U> {
 	onMouseLeftClickRelease?(position: Position): Action<T>;
 }
 
+export interface InterfaceDrawable<T> {
+	tick?(timestamp: number): void;
+	draw(ctx: CanvasRenderingContext2D, position?: Position): void;
+	getPosition(): Position;
+
+	onMouseMove?(position: Position): void;
+	onMouseLeftClick?(position: Position): Action<T> | undefined;
+	onMouseLeftClickRelease?(position: Position): Action<T>;
+}
+
 export class TextLabel<T, U> implements Drawable<T, U> {
 	position: Position;
 	text: string;
@@ -42,6 +52,54 @@ export class TextLabel<T, U> implements Drawable<T, U> {
 			slot.coord.x + (position?.x ?? 0) + this.position.x,
 			slot.coord.y + (position?.y ?? 0) + this.position.y
 		);
+	}
+
+	getPosition(): Position {
+		return this.position;
+	}
+}
+
+export class Button<T> implements InterfaceDrawable<T> {
+	position: Position;
+	size: Size;
+	action: Action<T>;
+	hovered: boolean = false;
+	radius: number;
+
+	constructor(position: Position, action: Action<T>, size: Size) {
+		this.position = position;
+		this.size = size;
+		this.radius = size.width/2;
+		this.action = action;
+	}
+
+	draw(ctx: CanvasRenderingContext2D, position?: Position): void {
+		ctx.fillStyle = this.hovered ? "red" : "blue";
+		ctx.beginPath();
+		ctx.arc(
+			this.position.x + (position?.x ?? 0) + this.radius,
+			this.position.y + (position?.y ?? 0) + this.radius,
+			this.radius,
+			0,
+			2 * Math.PI
+		);
+		ctx.fill();
+	}
+
+	private isHovered(position: Position): boolean {
+		if (position.x < this.position.x || position.x > this.position.x + this.size.width) return false;
+		if (position.y < this.position.y || position.y > this.position.y + this.size.height) return false;
+		return true;
+	}
+
+	onMouseMove(position: Position): void {
+		if (this.isHovered(position)) this.hovered = true;
+		else this.hovered = false;
+	}
+
+	onMouseLeftClick(position: Position): Action<T> | undefined {
+		if (!this.isHovered(position)) return
+		return this.action;
 	}
 
 	getPosition(): Position {
