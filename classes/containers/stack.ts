2247325dcd1045db2c2a197057eee16e960f7f13
xpakx <55586470+xpakx@users.noreply.github.com>
2025-09-10 20:05:20 +0200

    feat: add stack card container

---
+++ classes/containers/stack.ts
@@ -0,0 +1,109 @@
+import { Card, CardSlot, StartDataFn } from "../card.js";
+import { Position, Size } from "../game.js";
+import { CardContainer } from "./card-container.js";
+
+export type StackOrientation = "horizontal" | "vertical";
+
+export interface StackOptions {
+	orientation?: StackOrientation;
+	position?: Position;
+}
+
+export class Stack<T> implements CardContainer<T> {
+	cards: CardSlot<T>[] = [];
+	hoveredIndex: number = -1;
+	position: Position;
+	initFn?: StartDataFn<T>;
+
+	width: number;
+	orientation: StackOrientation;
+
+	constructor(width: number, opt?: StackOptions) {
+		const pos = opt?.position || {x: 0, y: 0};
+		this.orientation = opt?.orientation || "horizontal";
+		this.position = {
+			x: pos.x,
+			y: pos.y,
+		};
+		this.width = width;
+	}
+
+	setCards(cards: Card[], flipped?: boolean) {
+		const step = this.width/cards.length;
+		cards.forEach((card, i) => {
+			const x = this.orientation === "horizontal" ? this.position.x + i * step : this.position.x;
+			const y = this.orientation === "horizontal" ? this.position.y : this.position.y + i * step;
+			card.dealt = true; // DEBUG
+			if (flipped) card.flipped = true;
+			const slot = new CardSlot<T>({x, y}, i);
+			if (this.initFn) slot.setInitFunction(this.initFn);
+			slot.putCard(card);
+			this.cards.push(slot);
+		});
+	}
+
+	nextFrame(timestamp: number, ctx: CanvasRenderingContext2D): void {
+		this.cards.forEach((slot, i) => {
+			const card = slot.getCard();
+			if (!card) return;
+			const underCursor = i === this.hoveredIndex;
+			slot.tick(timestamp, underCursor);
+
+			slot.draw(ctx, {x: slot.coord.x, y: slot.coord.y});
+			
+		});
+	}
+
+	onMouseMove(position: Position): void {
+		this.hoveredIndex = this.mouseToIndex(position) ?? -1;
+	}
+
+	onMouseLeftClick(position: { x: number; y: number }): CardSlot<T> | undefined {
+		const idx = this.mouseToIndex(position);
+		if (idx !== undefined) return this.cards[idx];
+		return undefined;
+	}
+
+	private mouseToIndex(pos: Position): number | undefined {
+		if (!this.cards.length) return;
+
+		const step = this.width/this.cards.length;
+		const cardWidth = this.cards[0].getCard()!.size.width;
+		const cardHeight = this.cards[0].getCard()!.size.height;
+
+		const firstDimCorrect = this.orientation === "horizontal"
+			? pos.x >= this.position.x && pos.x <= this.position.x + this.width + cardWidth - this.width/this.cards.length
+			: pos.y >= this.position.y && pos.y <= this.position.y + this.width + cardHeight - this.width/this.cards.length;
+		if (!firstDimCorrect) return;
+		const secondDimCorrect = this.orientation === "horizontal"
+			? pos.y >= this.position.y && pos.y <= this.position.y + cardHeight
+			: pos.x >= this.position.x && pos.x <= this.position.x + cardWidth;
+		if (!secondDimCorrect) return;
+
+
+		const rel = this.orientation === "horizontal"
+			? pos.x - this.position.x
+			: pos.y - this.position.y;
+
+		if (rel < 0) return;
+
+
+		let idx = Math.floor(rel / step);
+		if (idx < 0) idx = 0;
+		if (idx >= this.cards.length) idx = this.cards.length - 1;
+
+		return idx;
+	}
+
+	onMouseLeftClickRelease(_position: Position): void {
+	}
+
+	getCards(): Card[] {
+		return this.cards.map(c => c.getCard()).filter(c => c !== undefined);
+	}
+
+	setDataFunction(fn: StartDataFn<T>) {
+		this.initFn = fn;
+	}
+}
+
