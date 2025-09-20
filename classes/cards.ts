4b46950c14ed65f20096810a782cb264e09e8cbd
xpakx <55586470+xpakx@users.noreply.github.com>
2025-09-20 20:40:03 +0200

    feat: add helper function to manage suits in traditional deck

--- classes/cards.ts
+++ classes/cards.ts
@@ -30,4 +30,45 @@ export class Cards {
 		const cardName = typeof card === "string" ? card : card.name;
 		return cardName.slice(0, cardName.length-1);
 	}
+
+	static isHeart(card: string | Card): boolean {
+		return Cards.getHeart() === this.getSuit(card);
+	}
+
+	static isDiamond(card: string | Card): boolean {
+		return Cards.getDiamond() === this.getSuit(card);
+	}
+
+	static isSpade(card: string | Card): boolean {
+		return Cards.getSpade() === this.getSuit(card);
+	}
+
+	static isClub(card: string | Card): boolean {
+		return Cards.getClub() === this.getSuit(card);
+	}
+
+	static getHeart(): string {
+		return "H";
+	}
+
+	static getDiamond(): string {
+		return "D";
+	}
+
+	static getSpade(): string {
+		return "S";
+	}
+
+	static getClub(): string {
+		return "C";
+	}
+
+	static toSuit(name: "hearts" | "spades" | "clubs" | "diamonds"): string {
+		switch(name) {
+			case "hearts": return Cards.getHeart();
+			case "spades": return Cards.getSpade();
+			case "clubs": return Cards.getClub();
+			case "diamonds": return Cards.getDiamond();
+		}
+	}
 }
